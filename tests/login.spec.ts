dc7f35a4471e945df84090d5062328a3063db606
Cory House <housecor@gmail.com>
2022-11-09 07:59:35 -0500

    Fix test

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,20 +1,8 @@
 import { test, expect } from "@playwright/test";
 
-test("homepage has title and links to intro page", async ({ page }) => {
+test("should use data from global setup", async ({ page }) => {
   await page.goto("http://127.0.0.1:5173");
 
-  // Should be logged in because the data from
-  await expect(page).toHaveTitle(/Playwright/);
-
-  // create a locator
-  const getStarted = page.getByRole("link", { name: "Get started" });
-
-  // Expect an attribute "to be strictly equal" to the value.
-  await expect(getStarted).toHaveAttribute("href", "/docs/intro");
-
-  // Click the get started link.
-  await getStarted.click();
-
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
+  // Should be logged in because it was set via global-setup.
+  await expect(page.getByText("fake token")).toBeVisible();
 });
