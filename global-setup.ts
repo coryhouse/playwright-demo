612c4fe3fbc16578318b77202b05ed389bc7e4d0
Cory House <housecor@gmail.com>
2022-11-09 07:55:47 -0500

    Fix setup

--- global-setup.ts
+++ global-setup.ts
@@ -1,11 +1,12 @@
-import { chromium } from "@playwright/test";
+import { chromium, expect } from "@playwright/test";
 
 // Log the user in and store their token for reuse in all tests.
 export default async function globalSetup() {
   const browser = await chromium.launch();
   const page = await browser.newPage();
   await page.goto("http://127.0.0.1:5173");
-  page.getByRole("button", { name: "Log in" }).click();
+  await page.getByRole("button", { name: "Log in" }).click();
+  await expect(page.getByText("fake token")).toBeVisible();
   await page.context().storageState({ path: "storageState.json" });
   await browser.close();
 }
